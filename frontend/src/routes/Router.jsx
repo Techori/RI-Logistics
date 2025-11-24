16298929a50e1c3d7c983fb2278ba9ea1617b1f4
Techori <harshrajsharma359@gmail.com>
2025-11-24 14:38:16 +0530

    Update Router.jsx

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -32,8 +32,8 @@ import BottomNavigation from "../components/common/BottomNavigation.jsx";
 import D2CSolutionsPage from "../pages/solutions/D2CSolutionsPage.jsx";
 import B2BSolutionsPage from "../pages/solutions/B2BSolutionsPage.jsx";
 import ThreePLSolutionsPage from "../pages/solutions/ThreePLSolutionsPage.jsx";
-import FleetOwnerRegistration from "../pages/Fleetownerregistration.jsx";
-import FranchisePartnerForm from "../pages/Franchisepartnerregistration.jsx";
+import FleetOwnerRegistration from "../pages/fleetownerregistration.jsx";
+import FranchisePartnerForm from "../pages/franchisepartnerregistration.jsx";
 const DashboardRouter = () => {
   const userRole = localStorage.getItem("userRole");
 
